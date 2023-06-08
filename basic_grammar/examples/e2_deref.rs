b36a8a40567d3dcf75d721d7e0d7084e871b7f88
y2gon2 <y2gon2@gmail.com>
2023-06-08 11:37:55 +0900

    Added: workspace - wikibook_kujira

---
+++ basic_grammar/examples/e2_deref.rs
@@ -0,0 +1,33 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> Self {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hell(name: &str) {
+    println!("Hi, {}", name);
+}
+
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("rsut"));
+    hell(&m);
+}
---
+++ wikibook_kujira/ReadMe.md
@@ -0,0 +1,5 @@
+## Codes based on the book "만들면서 배우는 러스트 프로그래밍"
+
+![image](./data/book.jpg)
+
+* Exercise codes from studying the book. 

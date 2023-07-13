736d29dd98d9185da5d79583ff98f55d5b913a5a
y2gon2 <y2gon2@gmail.com>
2023-07-13 15:26:41 +0900

    Removed:: all context in e04_raw_pointer.rs

--- basic_grammar/examples/e04_raw_pointer.rs
+++ basic_grammar/examples/e04_raw_pointer.rs
@@ -1,13 +1,13 @@
-fn print_vec(v: &Vec<&dyn ToString>) {
-    for i in v {
-        println!("{}", i.to_string());
-    }
-}
+// fn print_vec(v: &Vec<&dyn ToString>) {
+//     for i in v {
+//         println!("{}", i.to_string());
+//     }
+// }
 
 fn main() {
-    let v1 = vec![&"hello", &"world"];
-    print_vec(&v1);
+    // let v1 = vec![&"hello", &"world"];
+    // print_vec(&v1);
 
-    let v2: Vec<&str> = vec!["hello", "world"];
-    print_vec(&v2);
+    // let v2: Vec<&str> = vec!["hello", "world"];
+    // print_vec(&v2);
 }
